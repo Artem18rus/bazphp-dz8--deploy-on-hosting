2356ced065176c9bf4f528c0fbf58b19ae060468
Artem18rus <urart88@mail.ru>
2023-05-17 20:58:35 +0400

    5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let form = document.querySelector('.form');
 let funcEvent = (e) => {
   let inputLogin = form.querySelector('.inputLogin');
   if(inputLogin.value.length === 0) {
-    event.preventDefault();
+    e.preventDefault();
     inputLogin.style.background = '#fdbcb4';
   } else {
     inputLogin.style.background = '#FFFFFF';
